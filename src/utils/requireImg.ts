f23773a4a0e800ae27e4bc39a6c1b61d69ac4d6d
Zhangyaosheng <zhangyaosheng@evking.cn>
2025-07-02 16:03:42 +0800

    fix: 动态资源引入打包问题

--- src/utils/requireImg.ts
+++ src/utils/requireImg.ts
@@ -1,4 +1,10 @@
+// export const requireImg = (imgPath: string) => {
+//   const imgSrc = `/src/assets/${imgPath}`;
+//   return new URL(imgSrc, import.meta.url).href;
+// };
+// @ts-ignore
+const images = import.meta.glob("/src/assets/*", { eager: true, as: "url" });
+
 export const requireImg = (imgPath: string) => {
-  const imgSrc = `/src/assets/${imgPath}`;
-  return new URL(imgSrc, import.meta.url).href;
+  return images[`/src/assets/${imgPath}`];
 };
