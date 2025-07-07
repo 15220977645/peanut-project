1e76761396af46dbdd4a18d99122fceef537b8c0
Zhangyaosheng <zhangyaosheng@evking.cn>
2025-07-07 16:56:13 +0800

    fix: 处理图片

--- src/views/fullScreen/hngsChargeScreen/modules/components/mapDialog.vue
+++ src/views/fullScreen/hngsChargeScreen/modules/components/mapDialog.vue
@@ -58,10 +58,7 @@ function closeDialog() {
     >
       <div ref="mapDialogRef" class="dialog-map-content">
         <div class="close-icon" title="关闭全屏" @click="closeDialog">
-          <img
-            :src="'src/assets/fullScreen/hngsChargeScreen/close.svg'"
-            alt=""
-          />
+          <img src="/src/assets/fullScreen/hngsChargeScreen/close.svg" alt="" />
         </div>
         <Map
           :selTypeChn="''"
